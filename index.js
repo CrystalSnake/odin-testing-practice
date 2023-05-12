4e184d309fd2735f5e6b90ca3c267fdd4fdcf54e
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-05-12 11:34:20 +0400

    Add calculator tests and functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { number } = require('yargs');
+
 function capitalize(inputString) {
   return (
     inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase()
@@ -8,4 +10,30 @@ function reverse(inputString) {
   return inputString.split('').reverse().join('');
 }
 
-module.exports = { capitalize, reverse };
+function isNumber(firstOp, secondOp) {
+  if (typeof firstOp != 'number' || typeof secondOp != 'number') return false;
+  else return true;
+}
+
+const add = function (firstOp, secondOp) {
+  if (isNumber(firstOp, secondOp)) return firstOp + secondOp;
+  else return 'Arguments must be a number';
+};
+
+const subtract = function (firstOp, secondOp) {
+  if (isNumber(firstOp, secondOp)) return firstOp - secondOp;
+};
+
+const multiply = function (firstOp, secondOp) {
+  if (isNumber(firstOp, secondOp)) return firstOp * secondOp;
+};
+
+const divide = function (firstOp, secondOp) {
+  if (isNumber(firstOp, secondOp))
+    if (secondOp == 0) return `Error: can't divide by zero`;
+    else return firstOp / secondOp;
+};
+
+const calculator = { add, subtract, multiply, divide };
+
+module.exports = { capitalize, reverse, calculator };
--- index.test.js
+++ index.test.js
@@ -15,3 +15,27 @@ it('Reverse string simple', () => {
 it('Reverse string with punctuation', () => {
   expect(index.reverse('Hello world!')).toBe('!dlrow olleH');
 });
+
+it('Calculator input type check', () => {
+  expect(index.calculator.add('one', 2)).toBe('Arguments must be a number');
+});
+
+it('Calculator sum', () => {
+  expect(index.calculator.add(2, 5)).toBe(7);
+});
+
+it('Calculator subtract', () => {
+  expect(index.calculator.subtract(20, 5)).toBe(15);
+});
+
+it('Calculator multiply', () => {
+  expect(index.calculator.multiply(3, 4)).toBe(12);
+});
+
+it('Calculator divide', () => {
+  expect(index.calculator.divide(14, 2)).toBe(7);
+});
+
+it('Calculator divide by zero', () => {
+  expect(index.calculator.divide(14, 0)).toBe(`Error: can't divide by zero`);
+});
